2c86cefb644dd8b37b66505785b7b3a99fe422e8
Agha Muhammad Aslam <aghaaslam246@gmail.com>
2024-06-10 22:46:56 +0200

    Optimize bootstrap

--- Controller/app.js
+++ Controller/app.js
@@ -24,22 +24,6 @@ var blogRouter = require('./routes/blog'); // Import the blogRouter
 app.use('/', indexRouter);
 app.use('/blog', blogRouter); // add blogRouter to the app
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
 var fileUpload = require('express-fileupload');
 app.use(fileUpload({
   createParentPath: true
@@ -57,6 +41,22 @@ app.use(logger('combined', { stream: accessLogStream }));
 app.get('/blog', blogController.getAllPosts);
 app.post('/blog/newPost', blogController.createPost);
 app.get('/blog/:id', blogController.readPost);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
 
 
 module.exports = app;
